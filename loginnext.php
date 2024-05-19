03f6f20e131195373c8f4511058508a0d1ab12b0
kyleenicolas <167857373+kyleenicolas@users.noreply.github.com>
2024-05-19 19:27:40 +0800

    Add files via upload

---
+++ loginnext.php
@@ -0,0 +1,16 @@
+<?php
+    $username = ' NICOLAS';
+    $password = '123';
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        if ($_POST['username'] == $username && $_POST['password'] == $password) {
+            session_start();
+            $_SESSION['username'] = $_POST['username'];
+            header("Location: order.php");
+        } else {
+            header("Location: LOGIN.php?error=true");
+        }
+    } else {
+        header("Location: ORDERMENU.php");
+    }																																					
+?>
